62f88bb276513b3dcd5abecc528c8b0816d531b5
Jean <jeandavidvasquezyaruqui@gmail.com>
2022-10-25 13:39:59 -0500

    Deber de introduccion a la programacion(COntenidos,Git)

---
+++ src/Contenido.java
@@ -0,0 +1,28 @@
+public class Contenido {
+    public static void main(String[] args) {
+        System.out.println("Conceptos basicos de programacion");
+        System.out.println("Algunos conmceptos de programacion son:\n");
+        System.out.println("1.Paradigma de programación:Un paradigma de programación es la propuesta tecnológica adoptada e implementada como metodología para resolver problemas definidos");
+        System.out.println("2.Lenguaje de programación:Un  lenguaje de programación es un conjunto de palabras y expresiones que conforman un idioma artificial y que sirven para expresar en lenguaje humano las operaciones y procesos que debe realizar un computador.");
+        System.out.println("3.Algoritmo y programa informático:Un algoritmo es un término usado en ciencias de la computación, matemáticas y lógica.");
+        System.out.println("4.Compilación y depuración:La compilación es el procedimiento por el cual un programa traduce un lenguaje de programación a lenguaje máquina para que la computadora pueda interpretarlo.");
+        System.out.println("5.Identificadores y palabras reservadas:Los identificadores son palabras o símbolos léxicos que sirven para nombrar entidades y elementos dentro de un programa y que hacen posible la referencia a ellos. ");
+        System.out.println("6.Variables y constantes:Una variable es un espacio reservado en la memoria de un computador durante la ejecución de un programa y que contiene valores susceptibles de alterarse con el tiempo.");
+        System.out.println("7.Tipos de datos:Las variables y las constantes (y otras cosas) tienen otra característica que es el tipo de dato.");
+        System.out.println("8.Operadores de datos:Un operador de datos es un elemento del programa que se aplica a uno o varios operandos (variables, constantes, etc.) ");
+        System.out.println("9.Estructuras de control:Las estructuras de control permiten al programador poder modificar el flujo de ejecución de un programa.");
+        System.out.println("10.Comentarios:Los comentarios son anotaciones legibles y aclaratorias que el programador introduce dentro del código fuente de un programa.");
+        System.out.println("11.Diagramas de flujo:Un diagrama de flujo es un diagrama que describe un proceso, sistema o algoritmo informático. ");
+        System.out.println("12.Pseudocódigo:El pseudocódigo es una forma de expresar los distintos pasos que va a realizar un programa, de la forma más parecida a un lenguaje de programación.");
+        System.out.println("13.Compilador:Un compilador es un programa informático que traduce todo el código fuente de un proyecto de software a código máquina antes de ejecutarlo.");
+        System.out.println("14.Programa objeto:Es aquel programa que se encuentra en lenguaje máquina y que ya es ejecutable por ésta.");
+        System.out.println("15.Programa fuente:Programa fuente es una noción que se emplea como sinónimo de código fuente. Se trata de las instrucciones que un programa informático transmite a una computadora para que pueda ejecutarse.");
+        System.out.println("16.Código Binario:código formado solo por 0 y 1 que tiene como función codificar algo. Es la base de toda la lógica de la computadora. Todo se convierte, al final, en código binario.");
+        System.out.println("17.SSL (Secure Sockets Layer): es un tipo de seguridad estándar para garantizar que todos los datos estén encriptados en tránsito entre un sitio y un navegador.");
+        System.out.println("18.SOLID:(Single-responsibility, Open-closed, Liskov substitution, Interface segregation y Dependency Inversion principles.)");
+        System.out.println("19.Backdoor:Puerto creado por alguna aplicación maliciosa para establecer un canal de comunicación desde nuestro dispositivo hasta un punto en el que se puedan manipular nuestros datos o acciones.");
+        System.out.println("20.Backup:Copia de archivos en otro dispositivo para guardar información.");
+
+}
+
+}
---
+++ src/Git.java
@@ -0,0 +1,18 @@
+public class Git {
+    public static void main(String[] args) {
+        System.out.println("Que es Git");
+        System.out.println("Nos permite rastrear los cambios realizados en los archivos\n");
+        System.out.println("Para guardar en git usando el programna Intellij Idead los pasosson:\n");
+        System.out.println("Utilizamos primero los comandos git config que hace poner nuestras credenciales");
+        System.out.println("git config:puede ser usado para establecer una configuración específica de usuario, como el email, nombre de usuario y tipo de formato, etc.");
+        System.out.println("git config --global user.email(email@gmail.com)");
+        System.out.println("git config --global user.name(Jean)\n");
+        System.out.println("git init :reará un nuevo repositorio local GIT.\n");
+        System.out.println("git status:muestra la lista de los archivos que se han cambiado junto con los archivos que están por ser preparados o confirmados.\n");
+        System.out.println("git add .:git add se usa para agregar archivos al área de preparación.\n");
+        System.out.println("git commit:creará una instantánea de los cambios y la guardará en el directorio git.\n");
+        System.out.println("git remote add origin <host-or-remoteURL>:para conectar el repositorio local a un servidor remoto\n");
+        System.out.println("git push:se usa para enviar confirmaciones locales a la rama maestra del repositorio remoto.");
+
+    }
+}
