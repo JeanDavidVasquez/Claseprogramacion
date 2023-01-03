27c9a27ef819f50ef97539b40d41a95d9666560e
Jeanproject <116390051+Jeanproject@users.noreply.github.com>
2023-01-02 19:41:26 -0500

    Add files via upload

---
+++ src/Bimestre2/AlgotOrdenamiento/AlgotBurbuja.java
@@ -0,0 +1,29 @@
+package AlgotOrdenamiento;
+
+public class AlgotBurbuja {
+    public static void main(String[] args) throws InterruptedException{
+        int arreglo[]=new int[1000];
+        int indiceelem=0;
+        for (int indice = 1000; indice > 0; indice--) {
+            arreglo[indiceelem] = indice;
+            indiceelem++;
+        }
+        long inicio= System.currentTimeMillis();
+        Thread.sleep(2000);
+        for (int indice = 0; indice < arreglo.length-1; indice++) {
+            for (int indice2 = 0; indice2 < arreglo.length-1 ; indice2++) {
+                if (arreglo[indice2] > arreglo[indice2+1]) {
+                    int aux= arreglo[indice2];
+                    arreglo[indice2]=arreglo[indice2+1];
+                    arreglo[indice2+1]=aux;
+                }
+            }
+        }
+        for (int indice = 0; indice < arreglo.length; indice++) {
+            System.out.println(arreglo[indice]);
+        }
+        long fin = System.currentTimeMillis();
+        double tiempo= (double) ((fin-inicio)/1000);
+        System.out.println(tiempo+"En Segundos");
+    }
+}
